c2744f89efa9482614afe696a4203cdf7bd87fb5
Xavier Maximin <xavier.maximin.uk@gmail.com>
2024-04-22 11:35:31 -0400

    added mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+    before_action :set_post
+
+    def create
+        comment = @post.comments.create! params.required(:comment).permit(:content)
+        CommentsMailer.submitted(comment).deliver_later
+        redirect_to @post, notice: 'Comment was successfully created.'
+    end
+
+    private
+        def set_post
+            @post = Post.find(params[:post_id])
+        end
+    
 end
